111a163678f7ccfa4ceb165b820b4ed41d3abd21
Ryan Ngau <ryanngau03@gmail.com>
2026-01-22 20:32:52 +0800

    Rename main class

---
+++ src/main/java/Meow.java
@@ -0,0 +1,17 @@
+public class Duke {
+    public static void main(String[] args) {
+        String logo =
+                " __  __                       \n"
+                        + "|  \\/  |  ___   ___  _ __      \n"
+                        + "| |\\/| | / _ \\ / _ \\| '_ \\ \n"
+                        + "| |  | | | __/ |(_)| |_) |    \n"
+                        + "|_|  |_| \\___| \\___/| .__/ \n"
+                        + "                  |_|        \n";
+        System.out.println("Hello from\n" + logo);
+    }
+}
+String logo = " ____        _        \n"
+        + "|  _ \\ _   _| | _____ \n"
+        + "| | | | | | | |/ / _ \\\n"
+        + "| |_| | |_| |   <  __/\n"
+        + "|____/ \\__,_|_|\\_\\___|\n";
