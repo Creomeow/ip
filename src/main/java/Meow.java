a54de9d3de968150885768ccf3b77b9692368d77
Ryan Ngau <ryanngau03@gmail.com>
2026-01-22 21:11:50 +0800

    Add echo feature

--- src/main/java/Meow.java
+++ src/main/java/Meow.java
@@ -1,10 +1,26 @@
+import java.util.Scanner;
+
 public class Meow {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
         System.out.println("______________________________________");
         System.out.println("Hello! I'm Meow");
         System.out.println("What can I do for you?");
         System.out.println("______________________________________");
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println("______________________________________");
+
+        while (true) {
+            String input = sc.nextLine();
+            System.out.println(input);
+            System.out.println("______________________________________");
+
+            if (input.equals("bye")) {
+                System.out.println("______________________________________");
+                System.out.println("Bye. Hope to see you again soon!");
+                System.out.println("______________________________________");
+                break;
+            }
+        }
     }
 }
+
