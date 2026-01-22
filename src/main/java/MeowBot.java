2e02ede218e986c2c6659c88b5c849ec3738dfee
Ryan Ngau <ryanngau03@gmail.com>
2026-01-22 22:51:14 +0800

    Add output for mark and unmark

--- src/main/java/MeowBot.java
+++ src/main/java/MeowBot.java
@@ -16,6 +16,22 @@ public class MeowBot {
                 Meowtput.goodbye();
             } else if (input.equals("list")) {
                 Meowtput.showTasks(meow.getTasks());
+            } else if (input.startsWith("mark ")) {
+                int index = Integer.parseInt(input.substring(5)) - 1;
+                Task task = meow.getTasks().get(index);
+                task.markAsDone();
+
+                System.out.println("Nice! I've marked this task as done:");
+                System.out.println("  " + task);
+                Meowtput.line();
+            } else if (input.startsWith("unmark ")) {
+                int index = Integer.parseInt(input.substring(7)) - 1;
+                Task task = meow.getTasks().get(index);
+                task.markAsUndone();
+
+                System.out.println("OK, I've marked this task as not done yet:");
+                System.out.println("  " + task);
+                Meowtput.line();
             } else {
                 meow.addTask(input);
                 Meowtput.showAddedTask(input);
