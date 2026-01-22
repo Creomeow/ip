878fcc30fefff5a88de3b9a00b4a12ba498750f2
Ryan Ngau <ryanngau03@gmail.com>
2026-01-22 23:20:17 +0800

    Add output for Deadline, Event, ToDo classes

--- src/main/java/MeowBot.java
+++ src/main/java/MeowBot.java
@@ -32,9 +32,26 @@ public class MeowBot {
                 System.out.println("OK, I've marked this task as not done yet:");
                 System.out.println("  " + task);
                 Meowtput.line();
+            } else if (input.startsWith("todo ")) {
+                String desc = input.substring(5);
+                Task todo = new ToDo(desc);
+                meow.addTask(todo);
+                Meowtput.showAddedTask(todo, meow.getTasks().size());
+            } else if (input.startsWith("deadline ")) {
+                String[] parts = input.substring(9).split(" /by ");
+                Task deadline = new Deadline(parts[0], parts[1]);
+                meow.addTask(deadline);
+                Meowtput.showAddedTask(deadline, meow.getTasks().size());
+            } else if (input.startsWith("event ")) {
+                String eventFull = input.substring(6);
+                String[] parts = eventFull.split(" /from | /to ");
+                Task event = new Event(parts[0], parts[1], parts[2]);
+                meow.addTask(event);
+                Meowtput.showAddedTask(event, meow.getTasks().size());
             } else {
-                meow.addTask(input);
-                Meowtput.showAddedTask(input);
+                Task taskInput = new Task(input);
+                meow.addTask(taskInput);
+                Meowtput.showAddedTask(taskInput, meow.getTasks().size());
             }
         }
     }
