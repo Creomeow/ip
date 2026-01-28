87cfd1c49cca9c998f5fbf95cfa92ba8b42b23e0
Ryan Ngau <ryanngau03@gmail.com>
2026-01-28 23:17:59 +0800

    Update deadline class variable type

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,18 +1,20 @@
+import java.time.LocalDate;
+
 public class Deadline extends Task {
 
-    protected String by;
+    protected LocalDate by;
 
-    public Deadline(String description, String by) {
+    public Deadline(String description, LocalDate by) {
         super(description);
         this.by = by;
     }
 
     @Override
     public String toString() {
-        return "[D]" + super.toString() + " (by: " + by + ")";
+        return "[D]" + super.toString() + " (by: " + DateTimeUtil.formatDate(by) + ")";
     }
 
-    public String getBy() {
+    public LocalDate getBy() {
         return this.by;
     }
 }
